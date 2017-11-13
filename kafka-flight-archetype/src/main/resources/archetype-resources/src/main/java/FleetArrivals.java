c0d859a42bf0229a08b2313366ab2dabaf920ae4
Mike Spertus <mike@spertus.com>
2017-11-12 20:45:55 -0600

    Archetype for getting real-time flight data

---
+++ kafka-flight-archetype/src/main/resources/archetype-resources/src/main/java/FleetArrivals.java
@@ -0,0 +1,121 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+import java.net.Authenticator;
+import java.net.InetAddress;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ${package}.FleetArrivedResponse.FleetArrivedResult.Arrival;
+
+// Inspired by http://stackoverflow.com/questions/14458450/what-to-use-instead-of-org-jboss-resteasy-client-clientrequest
+public class FleetArrivals {
+	static class Task extends TimerTask {
+		private Client client;
+		public FleetArrivedResponse getFleetArrivedResponse() {
+			Invocation.Builder bldr = client.target("http://flightxml.flightaware.com/json/FlightXML2/FleetArrived?fleet=AAL&howMany=3&offset=0").request("application/json");
+			try {
+				return bldr.get(FleetArrivedResponse.class);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+			return null;  // Sometimes the web service fails due to network problems. Just let it try again
+		}
+
+		public FlightInfoExResponse getFlightInfoExResponse(String ident, int departureTime) {
+			String uri= String.format("http://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?ident=%s@%d&howMany=1&offset=0", ident, departureTime);
+			Invocation.Builder bldr = client.target(uri).request("application/json");
+			return bldr.get(FlightInfoExResponse.class);
+
+		}
+
+		// Adapted from http://hortonworks.com/hadoop-tutorial/simulating-transporting-realtime-events-stream-apache-kafka/
+		Properties props = new Properties();
+		String TOPIC = "flight-events";
+		KafkaProducer<String, String> producer;
+		
+		public Task() {
+			client = ClientBuilder.newClient();
+			// enable POJO mapping using Jackson - see
+			// https://jersey.java.net/documentation/latest/user-guide.html${symbol_pound}json.jackson
+			client.register(JacksonFeature.class); 
+			props.put("bootstrap.servers", bootstrapServers);
+			props.put("acks", "all");
+			props.put("retries", 0);
+			props.put("batch.size", 16384);
+			props.put("linger.ms", 1);
+			props.put("buffer.memory", 33554432);
+			props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+			props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+			producer = new KafkaProducer<>(props);
+		}
+
+		@Override
+		public void run() {
+			FleetArrivedResponse response = getFleetArrivedResponse();
+			if(response == null || response.getFleetArrivedResult() == null)
+				return;
+			ObjectMapper mapper = new ObjectMapper();
+
+			for(Arrival arrival : response.getFleetArrivedResult().getArrivals()) {
+				ProducerRecord<String, String> data;
+				try {
+					data = new ProducerRecord<String, String>
+					(TOPIC, 
+					 mapper.writeValueAsString(getFlightInfoExResponse(arrival.getIdent(), arrival.getActualdeparturetime())));
+					producer.send(data);
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+	public static class CustomAuthenticator extends Authenticator {
+
+		// Called when password authorization is needed
+		protected PasswordAuthentication getPasswordAuthentication() {
+
+			// Get information about the request
+			String prompt = getRequestingPrompt();
+			String hostname = getRequestingHost();
+			InetAddress ipaddr = getRequestingSite();
+			int port = getRequestingPort();
+
+			String username = "Put Your Username here"; // TODO
+			String password = "Put your password here"; // TODO
+
+			// Return the information (a data holder that is used by Authenticator)
+			return new PasswordAuthentication(username, password.toCharArray());
+
+		}
+
+	}
+
+	static String bootstrapServers = new String("localhost:9092");
+
+	public static void main(String[] args) {
+		if(args.length > 0)  // This lets us run on the cluster with a different kafka
+			bootstrapServers = args[0];
+		Authenticator.setDefault(new CustomAuthenticator());
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new Task(), 0, 60*1000);
+	}
+}
+
